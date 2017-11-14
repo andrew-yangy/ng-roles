5271203ff83daaac262ec32d4296655674bacc88
Andrew Yang <ayang@1-stop.biz>
2017-11-14 17:03:00 +1100

    setup demo

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RolesService} from "../../lib/src/service/roles.service";
+import {PermissionsService} from "../../lib/src/service/permissions.service";
 
 @Component({
   selector: 'demo-app',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  constructor() {
-  }
+    constructor(private rolesService: RolesService,
+                private permissionsService: PermissionsService) {
+        const permissions = [
+            {
+                key: "CREATE",
+                accessOnly: ["ADMIN"]
+            },
+            {
+                key: "SEARCH_CONSIST_ADM",
+                accessOnly: ["USER"]
+            }
+        ]
+        this.permissionsService.loadPermissions(permissions);
+        this.rolesService.loadRoles(['ADMIN']);
+    }
 }
